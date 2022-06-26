a69041fb19451599da6aff2d7f1146a9e542aa27
Fahim Uddin <fahimuddin.se@gmail.com>
2022-06-26 16:56:09 +0600

    Middleware

--- MY-EXPRESS/app.js
+++ MY-EXPRESS/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("I am middleware !");
+  next();
+});
+
 app.get("/", (request, response) => {
   response.send("Hello from express js!");
 });
